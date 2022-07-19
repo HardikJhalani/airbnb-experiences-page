e054dba4ccd01acb776970facd1e4cf3dbd6ccab
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2022-07-19 17:08:59 +0530

    started using props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Card from './components/Card';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import katie from './images/katie-zaferes.png'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+          img = {katie}
+          rating = "5.0"
+          reviewCount = {6}
+          country = "USA"
+          title = "Life lessons with Katie Zaferes"
+          price = {136}
+          status = 'SOLD OUT'
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,21 @@ import katie from '../images/katie-zaferes.png'
 import star from '../images/star.png'
 
 function Card(props) {
+  console.log(props);
   return (
     <div className='card'>
         <div className='card-image'>
-            <img src = {katie} alt = 'katie image' />
-            <p>SOLD OUT</p>
+            <img src = {props.img} alt = 'katie image' />
+            <p>{props.status}</p>
         </div>
         <div className='card-details'>
             <div className='detail-head'>
                 <img src = {star} alt = 'star image' />
-                <h5>5.0</h5>
-                <p>(6) &#x2022; USA</p>
+                <h5>{props.rating}</h5>
+                <p>{`(${props.reviewCount})`} &#x2022; {props.country}</p>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span>From $136 </span>/ person</p>
+            <p>{props.title}</p>
+            <p><span>From ${props.price} </span>/ person</p>
         </div>
     </div>  
   )
